d3e4865998244b38f0c8e387a88c89bc8ebb94d7
Taing korngmeng <korngmeng015@gmail.com>
2026-05-30 00:51:11 +0700

    Fix static files for Vercel

--- tax_calculation/settings.py
+++ tax_calculation/settings.py
@@ -121,10 +121,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "/static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
