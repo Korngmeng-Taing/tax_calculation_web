f73425948444e8f93d20e8cdf89d73538dd22fdb
Taing korngmeng <korngmeng015@gmail.com>
2026-05-30 19:05:29 +0700

    Setup PostgreSQL database

--- tax_calculation/settings.py
+++ tax_calculation/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+from dotenv import load_dotenv
+load_dotenv()
 import os 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,10 +79,11 @@ WSGI_APPLICATION = 'tax_calculation.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("POSTGRES_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
