5346ad0bd9729837560a8513bfc9bdd14c7dd314
Taing korngmeng <korngmeng015@gmail.com>
2026-05-30 19:14:57 +0700

    Fix DATABASE_URL config

--- tax_calculation/settings.py
+++ tax_calculation/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'tax_calculation.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("POSTGRES_URL"),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
