ca4e8c6a2f6cd11757bc94bd0849fdbebbd2ed22
Taing korngmeng <korngmeng015@gmail.com>
2026-06-02 00:15:17 +0700

    feat: add USD/KHR exchange rate banner to all calculator pages and home

--- tax_calculation/settings.py
+++ tax_calculation/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'calculator.context_processors.exchange_rate',
             ],
         },
     },
